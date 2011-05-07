60ec78265f90de85015218cc96af9cd93f7c10f5
Akshay Dodeja <dodeja@gmail.com>
2011-05-07 02:04:25 -0700

    rails 3 beta 1 app for FindEDM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findedm::Application.config.secret_token = '0e37f122aaf8dd1a0057c3bdbffb3bad6576e6e9120dee4991e7d7ab89ebefa487cf312b52524efc436f09f7eed790f9e61973467019ad7375b95ba845a2ec71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Findedm::Application.config.session_store :cookie_store, :key => '_findedm_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Findedm::Application.config.session_store :active_record_store
